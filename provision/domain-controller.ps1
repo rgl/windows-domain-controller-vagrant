3121c9be228fba10b9eda62c21b3980d4b7ae9b8
Rui Lopes <rgl@ruilopes.com>
2018-08-01 22:58:18 +0100

    make sure the Administrator account is properly configured before installing the AD

--- provision/domain-controller.ps1
+++ provision/domain-controller.ps1
@@ -2,8 +2,22 @@ $domain = 'example.com'
 $netbiosDomain = 'EXAMPLE'
 $safeModeAdminstratorPassword = ConvertTo-SecureString 'HeyH0Password' -AsPlainText -Force
 
+# make sure the Administrator has a password that meets the minimum Windows
+# password complexity requirements (otherwise the AD will refuse to install).
+echo 'Resetting the Administrator account password and settings...'
+Set-LocalUser `
+    -Name Administrator `
+    -AccountNeverExpires `
+    -Password $safeModeAdminstratorPassword `
+    -PasswordNeverExpires:$true `
+    -UserMayChangePassword:$true
+
+echo 'Disabling the Administrator account (we only use the vagrant account)...'
+Disable-LocalUser `
+    -Name Administrator
+
 echo 'Installing the AD services and administration tools...'
-Install-WindowsFeature AD-Domain-Services,RSAT-AD-AdminCenter,RSAT-ADDS-Tools                      
+Install-WindowsFeature AD-Domain-Services,RSAT-AD-AdminCenter,RSAT-ADDS-Tools
 
 echo 'Installing the AD forest (be patient, this will take more than 30m to install)...'
 Import-Module ADDSDeployment
