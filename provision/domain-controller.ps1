1ccf666d9cc86a8786c39462c7a463f2b269d1d1
Rui Lopes <rgl@ruilopes.com>
2019-04-13 11:43:30 +0100

    set the AD ForestMode and DomainMode to WinThreshold (Windows Server 2016)

--- provision/domain-controller.ps1
+++ provision/domain-controller.ps1
@@ -21,11 +21,13 @@ Install-WindowsFeature AD-Domain-Services,RSAT-AD-AdminCenter,RSAT-ADDS-Tools
 
 echo 'Installing the AD forest (be patient, this will take more than 30m to install)...'
 Import-Module ADDSDeployment
+# NB ForestMode and DomainMode are set to WinThreshold (Windows Server 2016).
+#    see https://docs.microsoft.com/en-us/windows-server/identity/ad-ds/active-directory-functional-levels
 Install-ADDSForest `
     -InstallDns `
     -CreateDnsDelegation:$false `
-    -ForestMode 'Win2012R2' `
-    -DomainMode 'Win2012R2' `
+    -ForestMode 'WinThreshold' `
+    -DomainMode 'WinThreshold' `
     -DomainName $domain `
     -DomainNetbiosName $netbiosDomain `
     -SafeModeAdministratorPassword $safeModeAdminstratorPassword `
