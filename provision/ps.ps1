10eb868d7833cddd9a8307ba4768eb0ea9024acc
Rui Lopes <rgl@ruilopes.com>
2019-07-02 21:05:36 +0100

    show managed exception

--- provision/ps.ps1
+++ provision/ps.ps1
@@ -7,12 +7,12 @@ param(
 )
 
 Set-StrictMode -Version Latest
-
 $ErrorActionPreference = 'Stop'
-
+$ProgressPreference = 'SilentlyContinue'
 trap {
     Write-Output "ERROR: $_"
     Write-Output (($_.ScriptStackTrace -split '\r?\n') -replace '^(.*)$','ERROR: $1')
+    Write-Output (($_.Exception.ToString() -split '\r?\n') -replace '^(.*)$','ERROR EXCEPTION: $1')
     Exit 1
 }
 
