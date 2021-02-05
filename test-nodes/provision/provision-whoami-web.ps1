b93467f324cd468e290204a512c88a6dfb27c2a1
Rui Lopes <rgl@ruilopes.com>
2021-02-05 09:00:34 +0000

    upgrade to whoami-web 0.0.2

--- test-nodes/provision/provision-whoami-web.ps1
+++ test-nodes/provision/provision-whoami-web.ps1
@@ -1,6 +1,7 @@
 $name = 'whoami-web'
-$url = 'https://github.com/rgl/whoami-web/releases/download/v0.0.1/whoami-web.zip'
-$hash = '577A99B7CAD8CD7F06DE1AAD2B21E43F33B330456BCB778D3A96051807954BD2'
+$installPath = "C:\$name"
+$url = 'https://github.com/rgl/whoami-web/releases/download/v0.0.2/whoami-web.zip'
+$hash = 'DE189EB6289083808D97C94C3155C8AD2668A821778C753DEA34A945FADD903E'
 
 # define the Install-Application function that downloads and unzips an application.
 Add-Type -AssemblyName System.IO.Compression.FileSystem
@@ -15,31 +16,11 @@ function Install-Application($name, $destinationPath, $url, $expectedHash, $expe
 }
 
 Write-Host "Downloading $name from $url..."
-Install-Application $name "C:\$name" $url $hash
+Install-Application $name $installPath $url $hash
 
-Write-Host "Creating the $name windows service..."
-$result = sc.exe create $name `
-    DisplayName= 'Who Am I?' `
-    binPath= "C:\$name\whoami.exe" `
-    obj= 'EXAMPLE\whoami$' `
-    start= auto
-if ($result -ne '[SC] CreateService SUCCESS') {
-    throw "sc.exe create failed with $result"
-}
-$result = sc.exe description $name `
-    'The introspective service'
-if ($result -ne '[SC] ChangeServiceConfig2 SUCCESS') {
-    throw "sc.exe description failed with $result"
-}
-$result = sc.exe failure $name `
-    reset= '0' `
-    actions= 'restart/60000'
-if ($result -ne '[SC] ChangeServiceConfig2 SUCCESS') {
-    throw "sc.exe failure failed with $result"
-}
-
-Write-Host "Starting the $name windows service..."
-Start-Service $name
+Write-Host "Installing $name..."
+cd $installPath
+.\install.ps1
 
 Write-Host "Creating the $name desktop shortcut..."
 [IO.File]::WriteAllText("c:\Users\Public\Desktop\$name.url", @"
