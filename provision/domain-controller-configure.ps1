1d520aaaccefdebfcba688b7ee92dc2070c6bc92
Rui Lopes <rgl@ruilopes.com>
2021-02-17 08:04:57 +0000

    wait for the domain controller to be ready right after the reboot

--- provision/domain-controller-configure.ps1
+++ provision/domain-controller-configure.ps1
@@ -1,15 +1,3 @@
-# wait until we can access the AD. this is needed to prevent errors like:
-#   Unable to find a default server with Active Directory Web Services running.
-while ($true) {
-    try {
-        Get-ADDomain | Out-Null
-        break
-    } catch {
-        Start-Sleep -Seconds 10
-    }
-}
-
-
 $adDomain = Get-ADDomain
 $domain = $adDomain.DNSRoot
 $domainDn = $adDomain.DistinguishedName
---
+++ provision/domain-controller-wait-for-ready.ps1
@@ -0,0 +1,10 @@
+# wait until we can access the AD. this is needed to prevent errors like:
+#   Unable to find a default server with Active Directory Web Services running.
+while ($true) {
+    try {
+        Get-ADDomain | Out-Null
+        break
+    } catch {
+        Start-Sleep -Seconds 10
+    }
+}
